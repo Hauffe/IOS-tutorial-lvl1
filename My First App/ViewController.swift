631660c020aeffaa22fb2b07b35c3bd95a0d6067
Alex <alex@email.com>
2020-09-14 19:45:48 -0300

    Training

--- My First App/ViewController.swift
+++ My First App/ViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        myLabel.text = "Change with code!"
+        myTextField.becomeFirstResponder()
+        lastName.becomeFirstResponder()
+        myTextField.delegate = self
+        lastName.delegate = self
+    }
+    @IBAction func buttonWasPressed(_ sender: Any) {
+        myLabel.text = "Hello \(myTextField.text!) \(lastName.text!)!"
+        view.endEditing(true)
     }
 
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+    
 }
 
