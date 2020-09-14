070ffd4b688f10abae9411b367ead15768f8c10c
Alex <alex@email.com>
2020-09-14 18:16:14 -0300

    Initial Commit

---
+++ My First App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My First App
+//
+//  Created by Alex on 14/09/20.
+//  Copyright © 2020 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
